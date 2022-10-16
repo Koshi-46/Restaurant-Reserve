8e666ba296c01f7c643d03f106ca17edd39eb604
Koshi-46 <kffk1739@yahoo.co.jp>
2022-10-16 11:30:03 +0900

    third

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    //
+    // \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+    // $session = \Stripe\Checkout\Session::create([
+    //     'payment_method_types' => ['card'],
+    //     'line_items' => [$lineItems],
+    //     'mode' => 'payment',
+    //     'success_url' => route('user.cart.success'),
+    //     'cancel_url' => route('user.cart.cancel'),
+    // ]);
+}
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', 'App\Http\Controllers\ShopController@index')->name('shop.index')
 Route::get('/search', 'App\Http\Controllers\ShopController@search')->name('shop.search');
 Route::post('/detail', 'App\Http\Controllers\ShopController@detail')->name('shop.detail');
 Route::post('/store', 'App\Http\Controllers\ReserveController@store')->name('reserve.store');
-Route::get('/mypage', 'App\Http\Controllers\ReserveController@index')->name('reserve.index');
+Route::get('/reserve', 'App\Http\Controllers\ReserveController@index')->name('reserve.index');
 Route::post('/change', 'App\Http\Controllers\ReserveController@detail')->name('reserve.change');
 Route::post('/update', 'App\Http\Controllers\ReserveController@update')->name('reserve.update');
 Route::post('/delete', 'App\Http\Controllers\ReserveController@delete')->name('reserve.delete');
@@ -36,6 +36,15 @@ Route::get('/nice', 'App\Http\Controllers\NiceController@index')->name('nice.ind
 
 Route::post('/like','App\Http\Controllers\NiceController@like')->name('shop.like');
 Route::post('/unlike','App\Http\Controllers\NiceController@unlike')->name('shop.unlike');
+
+Route::prefix('cart')->middleware('auth:users')->group(function(){
+    Route::get('/', [CartController::class, 'index'])->name('cart.index');
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');   
+    Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+    Route::get('checkout', [CartController::class, 'checkout'])->name('cart.checkout');
+    Route::get('success', [CartController::class, 'success'])->name('cart.success');
+    Route::get('cancel', [CartController::class, 'cancel'])->name('cart.cancel');
+});
 
 
 
